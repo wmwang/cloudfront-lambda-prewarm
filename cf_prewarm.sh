320ea4639401ef835cf34276c45f01f360011f64
wmwang <isosoman@gmail.com>
2023-08-14 11:39:51 +0800

    Update cf_prewarm.sh

--- cf_prewarm.sh
+++ cf_prewarm.sh
@@ -6,5 +6,5 @@ for file in `cat file.txt`
 do
 echo $file
 payload="{\"filename\":\"$file\",\"cloudfront_url\":\"d2qupf6sbg9i95.cloudfront.net\"}"
-aws lambda invoke --function-name cloudfront_prewarm --invocation-type Event --cli-binary-format raw-in-base64-out --payload $payload o.out
+aws lambda invoke --function-name cloudfront_prewarm --invocation-type Event --cli-binary-format raw-in-base64-out --payload $payload o.out 
 done
